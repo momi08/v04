637d7772468ec6139f245ff915aaa498f78d11cd
Tomislav Prstec <toprstec@vsite.hr>
2024-11-24 13:56:06 +0100

    app.h changed.

--- app/app.h
+++ app/app.h
@@ -12,27 +12,35 @@ namespace vsite::oop::v4
     };
 
     class insect: public animal {
+    public:    
         unsigned int legs() const override;
     };
+
     class cockroach : public insect {
+    public:    
         std::string species() const override;
     };
 
     class spider : public animal {
+    public:    
         unsigned int legs() const override;
     };
     class tarantula : public spider {
+    public:    
         std::string species() const override;
     };
 
     class bird : public animal {
+    public:    
         unsigned int legs() const override;
     };
     class sparrow : public bird {
+    public:    
         std::string species() const override;
     };
 
     class leg_counter {
+    private:    
         unsigned legCount = 0;
     public:
         std::string add_animal(animal* anim);
